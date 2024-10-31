29898eef1e95c956e86d98281db8da33ae36f992
Davis-don <davismugoikou@gmail.com>
2024-10-31 10:17:00 +0300

    added header section

---
+++ client/src/App.jsx
@@ -0,0 +1,14 @@
+
+import './App.css'
+import Homescreen from './assets/Pages/Homescreen/Homescreen'
+
+function App() {
+
+  return (
+   <div className='app'>
+   <Homescreen/>
+   </div>
+  )
+}
+
+export default App
---
+++ client/src/assets/Pages/Homescreen/Homescreen.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Header from '../../Layout/Header/Header'
+import './homescreen.css'
+
+function Homescreen() {
+  return (
+    <div className='overall-homescreen-container'>
+        <Header/>
+        </div>
+  )
+}
+
+export default Homescreen
